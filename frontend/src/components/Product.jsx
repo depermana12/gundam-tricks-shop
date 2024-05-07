5a650199b3b487a0123e8b84afdfbd231e9540e3
depermana12 <deddiapermana97@gmail.com>
2024-05-08 00:43:49 +0700

    add rating component: implement react router

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,18 +1,24 @@
+import { Link } from "react-router-dom"
 import { Card } from "react-bootstrap"
+import Rating from "./Rating"
 
 const Product = ({product}) => {
     
   return (
     <Card className="my-3 p-3 rounded">
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
             <Card.Img src={product.image} variant="top" />
-        </a>
+        </Link>
         <Card.Body>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Title as="div">
                     <strong>{product.name}</strong>
                 </Card.Title>
-            </a>
+            </Link>
+            <Card.Text as="div">
+                <Rating value={ product.rating } text={`${product.numReviews} reviews`} />
+            </Card.Text>
+
             <Card.Text as="h3">
                 {new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", maximumFractionDigits: 0, }).format(product.price)}
             </Card.Text>
