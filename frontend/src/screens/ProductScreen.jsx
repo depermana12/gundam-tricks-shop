b42c3e5d9835f732a378dd77982789caf3a5eeac
depermana12 <deddiapermana97@gmail.com>
2024-05-08 19:57:55 +0700

    add rating component: implement react router

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,69 @@
+// get id from url using hook useParams
+import { Link, useParams } from "react-router-dom"
+import { Row, Col, Image, ListGroup, Card, Button} from "react-bootstrap"
+import products from "../products"
+import Rating from "../components/Rating"
+
+function ProductScreen() {
+    const formatCurrency = (price) => new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR", maximumFractionDigits: 0, }).format(price)
+
+    const { id: productId} = useParams()
+    const product = products.find((p)=> p._id === productId)
+
+  return (
+    <>
+    <Link className="btn btn-light my-3" to="/">
+        Back
+    </Link>
+    <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={4}>
+            <ListGroup variant="flush">
+                <ListGroup.Item>
+                    <h3>{product.name}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    Price: {formatCurrency(product.price)}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                {product.description}
+                </ListGroup.Item>
+            </ListGroup>
+        </Col>
+        <Col md={3}>
+            <Card>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>
+                        <Row>
+                            <Col>Price:</Col>
+                            <Col>
+                            <strong>{formatCurrency(product.price)}</strong>
+                            </Col>
+                        </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Row>
+                            <Col>Status:</Col>
+                            <Col>
+                            <strong>{product.countInStock > 0 ? "In Stock" : "Out of Stock"}</strong>
+                            </Col>
+                        </Row>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
+                            Add to Cart
+                        </Button>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Card>
+        </Col>
+    </Row>
+    </>
+  )
+}
+export default ProductScreen
