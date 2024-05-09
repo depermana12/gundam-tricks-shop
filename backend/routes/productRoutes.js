2b6553c0ab26dbb7988707e9b4cfdaac8da52a02
depermana12 <deddiapermana97@gmail.com>
2024-05-09 21:45:42 +0700

    add error handler middleware and routes controller

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+import { getProduct, getProductById } from "../controller/productController.js";
+
+router.route("/").get(getProduct);
+router.route("/:id").get(getProductById);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,21 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorHandler.js";
 
 connectDB();
 const port = process.env.PORT;
 const app = express();
+
 app.use(cors({ origin: "http://localhost:3000" }));
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log("Server running, listening to port 5000"));
